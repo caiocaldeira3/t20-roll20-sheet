fd0ec5942ccbd668a77ae2dd08092f6a0aee0e7c
Caio Caldeira <caio.caldeira3@gmail.com>
2021-03-25 01:19:21 -0300

    Adicionar script para repetições de poderes

---
+++ repeating_powers.py
@@ -0,0 +1,22 @@
+import pyperclip as pyclip
+
+from base import repeat, describe
+
+print("Cole aqui o ID do poder que você quer repetir:")
+print("(Caso CTRL-V não cole o ID do poder recomendo testar CTRL+SHIFT+V)")
+
+ability_id = input()
+
+power_name = repeat("repeating_powers", ability_id, "namepower")
+power_description = repeat("repeating_powers", ability_id, "powerdescription")
+
+recommended = describe(power_name, power_description)
+
+print(f"Macro para o nome da habilidade: {power_name}")
+print(f"Macro para o nome da habilidade: {power_description}")
+
+print(f"Macro recomendada: {recommended}")
+
+print("Caso todos os pacotes recomendados tenham sido baixados a macro recomendada já deve ter sido automáticamente copiada para o seu clipboard.")
+print("Para salvar a macro basta apertar CTRL+V no espaço de macro desejado.")
+pyclip.copy(recommended)
