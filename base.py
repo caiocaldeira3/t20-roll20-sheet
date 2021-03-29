9f341277cd928b8eaf6eacee9cfb1ade6192b8a8
caiocaldeira3 <caio.caldeira3@gmail.com>
2021-03-29 21:22:53 +0000

    Prettyfy base.py

--- base.py
+++ base.py
@@ -3,83 +3,119 @@ import regex as re
 from typing import Sequence, Union
 
 def repeat(rpt_type: str, rpt_id: str, rpt_attr: str) -> str:
-	return f"@{{{rpt_type}_{rpt_id}_{rpt_attr}}}"
+    return f"@{{{rpt_type}_{rpt_id}_{rpt_attr}}}"
 
 def describe(rpt_name: str, rpt_desc: str) -> str:
-	return f"**{rpt_name}**: {rpt_desc}"
+    return f"**{rpt_name}**: {rpt_desc}"
 
 substitute_regex = r"@{(?!character_name)(.*?)}"
 def sub_template(action_template: str, action_id: str) -> str:
-	return re.sub(substitute_regex, action_template, "@{_{action_id}_)")
+    return re.sub(substitute_regex, action_template, "@{_{action_id}_)")
 
-def padronize(rpt_type: str, rpt_id: str, rpt_attrs: Union[Sequence[str], str]) -> list[str]:
-	if not isinstance(rpt_attrs, Sequence):
-		rpt_attrs = [rpt_attrs]
-	elif isinstance(rpt_attrs, str):
-		rpt_attrs = [rpt_attrs]
+def padronize(
+    rpt_type: str, rpt_id: str, rpt_attrs: Union[Sequence[str], str]
+) -> list[str]:
+    if not isinstance(rpt_attrs, Sequence):
+        rpt_attrs = [rpt_attrs]
+    elif isinstance(rpt_attrs, str):
+        rpt_attrs = [rpt_attrs]
 
-	results = [
-		repeat(rpt_type, rpt_id, rpt_attr) for rpt_attr in rpt_attrs
-	]
-	return results
+    results = [
+        repeat(rpt_type, rpt_id, rpt_attr) for rpt_attr in rpt_attrs
+    ]
+    return results
 
 def rpt_ability(ability_id: str, rpt_attrs: Union[Sequence[str], str]) -> list[str]:
-	if not isinstance(rpt_attrs, Sequence):
-		rpt_attrs = [rpt_attrs]
-	elif isinstance(rpt_attrs, str):
-		rpt_attrs = [rpt_attrs]
+    if not isinstance(rpt_attrs, Sequence):
+        rpt_attrs = [rpt_attrs]
+    elif isinstance(rpt_attrs, str):
+        rpt_attrs = [rpt_attrs]
 
-	ability_attrs = padronize(
-		"repeating_abilities", ability_id, rpt_attrs
-	)
-	return ability_attrs
+    ability_attrs = padronize(
+        "repeating_abilities", ability_id, rpt_attrs
+    )
+    return ability_attrs
 
 def rpt_power(power_id: str, rpt_attrs: Union[Sequence[str], str]) -> list[str]:
-	if not isinstance(rpt_attrs, Sequence):
-		rpt_attrs = [rpt_attrs]
-	elif isinstance(rpt_attrs, str):
-		rpt_attrs = [rpt_attrs]
+    if not isinstance(rpt_attrs, Sequence):
+        rpt_attrs = [rpt_attrs]
+    elif isinstance(rpt_attrs, str):
+        rpt_attrs = [rpt_attrs]
 
-	power_attrs = padronize(
-		"repeating_powers", power_id, rpt_attrs
-	)
-	return power_attrs
+    power_attrs = padronize(
+        "repeating_powers", power_id, rpt_attrs
+    )
+    return power_attrs
 
 def rpt_attack(attack_id: str, rpt_attrs: Union[Sequence[str], str]) -> list[str]:
-	if not isinstance(rpt_attrs, Sequence):
-		rpt_attrs = [rpt_attrs]
-	elif isinstance(rpt_attrs, str):
-		rpt_attrs = [rpt_attrs]
+    if not isinstance(rpt_attrs, Sequence):
+        rpt_attrs = [rpt_attrs]
+    elif isinstance(rpt_attrs, str):
+        rpt_attrs = [rpt_attrs]
 
-	attack_attrs = padronize(
-		"repeating_attacks", attack_id, rpt_attrs
-	)
-	return attack_attrs
+    attack_attrs = padronize(
+        "repeating_attacks", attack_id, rpt_attrs
+    )
+    return attack_attrs
 
-def rpt_spell(spell_id: str, spell_circle: str, rpt_attrs: Union[Sequence[str], str]) -> list[str]:
-	if not isinstance(rpt_attrs, Sequence):
-		rpt_attrs = [rpt_attrs]
-	elif isinstance(rpt_attrs, str):
-		rpt_attrs = [rpt_attrs]
+def rpt_spell(
+    spell_id: str, spell_circle: str, rpt_attrs: Union[Sequence[str], str]
+) -> list[str]:
+    if not isinstance(rpt_attrs, Sequence):
+        rpt_attrs = [rpt_attrs]
+    elif isinstance(rpt_attrs, str):
+        rpt_attrs = [rpt_attrs]
 
-	spell_attrs = padronize(
-		f"repeating_spells{spell_circle}", spell_id, rpt_attrs
-	)
-	return spell_attrs
+    spell_attrs = padronize(
+        f"repeating_spells{spell_circle}", spell_id, rpt_attrs
+    )
+    return spell_attrs
 
 def create_attack_template(attack_id: str) -> str:
-	clean_template = "&{template:t20-attack}{{character=@{character_name}}}{{attackname=@{nomeataque}}}{{attackroll=[[1d20cs>@{margemcriticoataque}+[[@{ataquepericia}]]+@{bonusataque}]]}} {{damageroll=[[@{danoataque}+@{modatributodano}+@{danoextraataque}]]}} {{criticaldamageroll=[[@{danocriticoataque}+@{modatributodano}+@{danoextraataque}]]}}{{typeofdamage=@{ataquetipodedano}}}{{description=@{ataquedescricao}}}"
-	return re.sub(r"@{(?!character_name)(.*?)}", lambda x: rpt_attack(attack_id, x.group(1))[0], clean_template)
+    clean_template = (
+        "&{template:t20-attack}{{character=@{character_name}}}{{attackname="
+        "@{nomeataque}}}{{attackroll=[[1d20cs>@{margemcriticoataque}+"
+        "[[@{ataquepericia}]]+@{bonusataque}]]}} {{damageroll=[[@{danoataque}+"
+        "@{modatributodano}+@{danoextraataque}]]}} {{criticaldamageroll="
+        "[[@{danocriticoataque}+@{modatributodano}+@{danoextraataque}]]}}"
+        "{{typeofdamage=@{ataquetipodedano}}}{{description=@{ataquedescricao}}}"
+    )
+    return re.sub(
+        r"@{(?!character_name)(.*?)}",
+        lambda x: rpt_attack(attack_id, x.group(1))[0],
+        clean_template
+    )
 
 def create_ability_template(ability_id: str) -> str:
-	clean_template="&{template:t20-info}{{infoname=@{nameability}}}{{description=@{abilitydescription}}}"
-	return re.sub(r"@{(?!character_name)(.*?)}", lambda x: rpt_ability(ability_id, x.group(1))[0], clean_template)
+    clean_template = (
+        "&{template:t20-info}{{infoname=@{nameability}}}{{description="
+        "@{abilitydescription}}}"
+    )
+    return re.sub(
+        r"@{(?!character_name)(.*?)}",
+        lambda x: rpt_ability(ability_id, x.group(1))[0], clean_template
+    )
 
 def create_power_template(power_id: str) -> str:
-	clean_template="&{template:t20-info}{{infoname=@{namepower}}}{{description=@{powerdescription}}}"
-	return re.sub(r"@{(?!character_name)(.*?)}", lambda x: rpt_power(power_id, x.group(1))[0], clean_template)
+    clean_template = (
+        "&{template:t20-info}{{infoname=@{namepower}}}"
+        "{{description=@{powerdescription}}}"
+    )
+    return re.sub(
+        r"@{(?!character_name)(.*?)}",
+        lambda x: rpt_power(power_id, x.group(1))[0], clean_template
+    )
 
 
 def create_spell_template(spell_id: str, spell_circle: str) -> str:
-	clean_template = "&{template:spell}{{character=@{character_name}}}{{spellname=@{namespell}}}{{type=@{spelltipo}}}{{execution=@{spellexecucao}}}{{duration=@{spellduracao}}}{{range=@{spellalcance}}}{{targetarea=@{spellalvoarea}}}{{resistance=@{spellresistencia}}}{{description=@{spelldescription}}}{{cd=@{spellcd}}}"
-	return re.sub(r"@{(?!character_name)(.*?)}", lambda x: rpt_spell(spell_id, spell_circle, x.group(1))[0], clean_template)
+    clean_template = (
+        "&{template:spell}{{character=@{character_name}}}{{spellname="
+        "@{namespell}}}{{type=@{spelltipo}}}{{execution=@{spellexecucao}}}{{duration="
+        "@{spellduracao}}}{{range=@{spellalcance}}}{{targetarea=@{spellalvoarea}}}"
+        "{{resistance=@{spellresistencia}}}{{description=@{spelldescription}}}{{cd="
+        "@{spellcd}}}"
+    )
+    return re.sub(
+        r"@{(?!character_name)(.*?)}",
+        lambda x: rpt_spell(spell_id, spell_circle, x.group(1))[0], clean_template
+    )
